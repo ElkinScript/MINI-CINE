274224b4508d2ada950afaca38af01ad4517fcdc
Elkin Moyon <elkinmoyon2004@gmail.com>
2025-06-15 22:45:34 -0500

    add movie API integration and update templates for movie listings

--- core/dashboard/urls.py
+++ core/dashboard/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from core.dashboard.views import DashboardView, ContactView
+from core.dashboard.views.api_movie import CurrentMoviesView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
 
     #contact
     path('contact/', ContactView.as_view(), name='contact'),
+
+    # API Movie
+    path('api-movie/', CurrentMoviesView.as_view(), name='api_movie'),
 ]
--- core/dashboard/views/dashboard/dashboard.py
+++ core/dashboard/views/dashboard/dashboard.py
@@ -1,4 +1,6 @@
+import requests
 from django.views.generic import TemplateView
+
 
 class DashboardView(TemplateView):
     def get_template_names(self):
@@ -6,5 +8,9 @@ class DashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Panel de administración'
+        API_KEY = "bba4a202b6ee1095d6223971aacb8752"
+        url = f"https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}&language=es-ES&page=1"
+        response = requests.get(url)
+        data = response.json()
+        context["peliculas"] = data.get("results", [])
         return context
